eecd08347ffc916fd559da49851977323f8ccf20
Arc of Chemung Schuyler <developer@arcofchemung.org>
2020-08-14 14:08:10 -0600

    Initial Commit

---
+++ Mealswipe/MealswipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealswipeApp.swift
+//  Mealswipe
+//
+//  Created by Brock May on 8/14/20.
+//
+
+import SwiftUI
+
+@main
+struct MealswipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
